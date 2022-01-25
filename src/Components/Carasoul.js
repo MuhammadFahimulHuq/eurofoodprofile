63a4448e798118d85b32f921b8d0811b530deb2f
unknown <fahimul.huq85@gmail.com>
2022-01-25 11:31:45 +0600

    carasoul uploaded

--- src/Components/Carasoul.js
+++ src/Components/Carasoul.js
@@ -2,6 +2,7 @@ import React from 'react';
 import mainbackground from '../Asset/images/mainbackground.jpg'
 import mainbackground1 from '../Asset/images/mainbackground1.jpg'
 import { Carousel } from 'react-bootstrap';
+import './Carasoul.css'
 export const Carasoul = () => {
   return <div > 
 <Carousel indicators={false}>
@@ -14,8 +15,8 @@ export const Carasoul = () => {
       style={{ filter: 'blur(2px)', webkitFilter: 'blur(2px)'}}
     />
     <Carousel.Caption>
-      <h2 style={{fontSize:'70px'}}>ENSURING THE HIGHEST QUALITY</h2>
-      <p style={{fontSize:'30px'}}>Wholesale suppliers to the catering industry, restaurants
+      <h2 className="carasoul-header">ENSURING THE HIGHEST QUALITY</h2>
+      <p className="carasoul-subtitle">Wholesale suppliers to the catering industry, restaurants
 and specialist supermarkets.</p>
     </Carousel.Caption>
   </Carousel.Item>
@@ -28,8 +29,8 @@ and specialist supermarkets.</p>
     />
 
     <Carousel.Caption>
-    <h2 style={{fontSize:'70px'}}>ENSURING THE HIGHEST QUALITY</h2>
-      <p style={{fontSize:'30px'}}>Wholesale suppliers to the catering industry, restaurants
+    <h2 className='carasoul-header'>ENSURING THE HIGHEST QUALITY</h2>
+      <p className='carasoul-subtitle'>Wholesale suppliers to the catering industry, restaurants
 and specialist supermarkets.</p>
     </Carousel.Caption>
   </Carousel.Item>
